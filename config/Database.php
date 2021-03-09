766118097b328e683330a8392ef8c1d724bda418
Samiur-UTI <allpraisethelord@gmail.com>
2021-03-09 21:34:25 +0600

    init

---
+++ config/Database.php
@@ -0,0 +1,22 @@
+<?php 
+    class database {
+        //Params
+        private $host = 'localhost';
+        private $db = 'phpBlog';
+        private $user = 'root';
+        private $password = '';
+        private $conn;
+
+        public function connect(){
+            $this->conn = null;
+            try {
+                $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db,$this->user,$this->password);
+                $this->conn = setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                //throw $th;
+                echo 'Error connection: '.$e->getMessage();
+            }
+            return $this->conn;
+        }
+    }
+?>
---
+++ models/Post.php
@@ -0,0 +1,82 @@
+<?php 
+    class Post{
+        //DB params
+        private $conn;
+        private $table = 'posts';
+        //Post params
+        private $id;
+        private $title;
+        private $body;
+        private $created_at;
+
+        //Establish constructor with DB
+        public function __construct($db){
+            $this->conn = $db;
+        }
+        //Methods
+        public function create(){
+            $query = 'INSERT INTO '
+                        .$this->table.
+                     '(title,body) VALUES (:title,:body)';
+            $stmt = $this->conn->prepare($query);
+            
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->post = htmlspecialchars(strip_tags($this->post));
+
+            $stmt->bindParam('title', $this->title);
+            $stmt->bindParam('body', $this->body);
+
+            $stmt->execute();
+
+            if($stmt->execute()){
+                return true;
+            } else {
+                return false;
+            }
+        
+        }
+        public function read(){
+            $query = 'SELECT * FROM '.$this->table;
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt;
+        }
+        public function read_single(){
+            $query = 'SELECT * FROM '.$this->table.'WHERE id = ?';
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1,$this->id);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->id = $row['id'];
+            $this->title = $row['title'];
+            $this->body = $row['body'];
+        }
+        public function update(){
+            $query = 'UPDATE TABLE '.$this->table.
+                     'SET title=:title, body:body 
+                     WHERE id= :id';
+            $stmt = $this->conn->prepare($query);
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->post = htmlspecialchars(strip_tags($this->post));
+
+            $stmt->bindParam('title', $this->title);
+            $stmt->bindParam('body', $this->body);
+            $stmt->bindParam('id', $this->id);
+            $stmt->execute();
+            if($stmt->execute()){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public function delete(){
+            $query = 'DELETE FROM '.$this->table.'WHERE id=?';
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1,$this->id);
+            $stmt->execute();
+        }
+    }
+
+
+?>
